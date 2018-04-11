0e80f0a9be2cb4f588dc0f0b3227e4828b6d6505
Carlos <c.feliciano79@gmail.com>
2018-04-11 12:55:11 -0700

    added the config file and imported the Config class into __init__.py so we can read it and apply it when doing our WTForms.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask # importing Flask from flask package installation
+from config import Config # importing config class from config.py
+
 
 app = Flask(__name__)
+app.config.from_object(Config) # config items can be access with a dictionary syntax from app.config
 
 from app import routes
 
